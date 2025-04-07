216329cbf3eecfd2860ba2d1caf0c6761ebb036d
George Cubas <georgeraulc@gmail.com>
2025-04-07 13:12:34 -0500

    Update app.py

--- digital_resume_streamlit/app.py
+++ digital_resume_streamlit/app.py
@@ -5,7 +5,7 @@ from PIL import Image
 
 
 # --- PATH SETTINGS ---
-current_dir = Path(__file__).parent if "__file__" in locals() else Path.cwd()
+current_dir = Path(__file__).resolve().parent
 css_file = current_dir / "styles" / "main.css"
 resume_file = current_dir / "assets" / "CV.pdf"
 profile_pic = current_dir / "assets" / "profile-pic.png"
